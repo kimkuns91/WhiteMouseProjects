965eecfc852348039846c14490e658312a4c2d88
Kun Woo's Mac Mini <kunwookim@Kunui-Macmini.local>
2023-08-23 17:17:43 +0900

    카테고리 수정

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -4,11 +4,11 @@ const Category = ({ category, handleCategory })=>{
             <option value="카테고리 없음">카테고리 없음</option>
             <option value="React">React</option>
             <option value="NodeJS">NodeJS</option>
+            <option value="AWS">AWS</option>
             <option value="Javascript">Javascript</option>
             <option value="Java">Java</option>
             <option value="HTML">HTML</option>
             <option value="CSS">CSS</option>
-            <option value="AWS">AWS</option>
         </select>
     )
 }
